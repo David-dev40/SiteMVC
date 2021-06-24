8a48d2025daa3c01cc9aacfe86b510dd682a9041
David-dev40 <83361653+David-dev40@users.noreply.github.com>
2021-06-24 23:41:04 +0200

    maj comments actor

--- view/template.php
+++ view/template.php
@@ -8,7 +8,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width" />
-        <link rel="stylesheet" type="text/css" media="screen" href="./public/style.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="/SiteMVC/public/style.css" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Martel%7cOpen+Sans&display=swap" />
         <link rel="icon" href="public/img/wicon_gbaf.png" />
@@ -18,14 +18,14 @@
     <header>
         <?php ob_start(); ?> 
         <header id="header_form">
-            <div><a href="index.php?action=pageHome"><img id="logoGBAF" src ="public/img/logo_gbaf.png" alt="logo de GBAF" /></div>
+            <div><img id="logoGBAF" src ="public/img/logo_gbaf.png" alt="logo de GBAF" /></div>
             <h1>Le Groupement Banque Assurance Français</h1>
         </header>
         <?php $headerOut = ob_get_clean(); ?>
 
         <?php ob_start(); ?>
         <header id="header">
-            <div><a href="index.php?action=pageHome"><img id="logo" src="public/img/logo_gbaf.png" alt="logo de GBAF" /></div>
+        <p><div><img id="logo" src="public/img/logo_gbaf.png" alt="logo de GBAF" /></p></div>
             <div id="user">
                 <div class="fas fa-user-tie fa-2x"></div>
                 <div id="userLink">
@@ -48,7 +48,7 @@
         </main>
        
         <footer>
-            <p><a href="index.php?action=mentionsLegales">Mentions légales</a><a> | </a><a href="index.php?action=contact">Contact</a></p>
+            <p><a href="index.php?action=mentionsLegales">Mentions légales</a> | <a href="#">Contact</a></p>
         </footer>
 </body>
 <?php //dd($_GET,$_POST,$_REQUEST);?>
